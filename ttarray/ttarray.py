e5d3cf5c7993942046b4ba36f1049d533860d87b
Michael Sonner <sonnerm@gmail.com>
2022-03-10 11:53:06 +0100

    upgraded shape,cluster, ... to properties

--- ttarray/ttarray.py
+++ ttarray/ttarray.py
@@ -31,62 +31,86 @@ def implement_ufunc(ufunc,method,selector=None):
 
 def _product(seq):
     return reduce(lambda a,b:a*b,seq,1)
-def _flatten_mps(mpsl):
-    pass
+def _flatten_ttslice(seq):
+    ret=[]
+    for s in seq:
+        if isinstance(s,TensorTrainSlice):
+            ret.extend(s._data)
+        ret.append(s)
+    return ret
+
 class TensorTrainBase:
     # So far this is just for isinstance
     pass
 
 class _TensorTrainSliceData:
-    def __init__(self,mps):
-        self.mps=mps
+    def __init__(self,tts):
+        self._tts=tts
     def __getitem__(self,ind):
         if isinstance(ind,slice):
             if ind.step==1:
-                return self.mps.__class__(self.mps._data[ind])
+                return self._tts.__class__(self._tts._data[ind])
             else:
                 raise IndexError("Slicing with a step not equal to one not supported for TensorTrains")
         else: #single index i guess
-            return self.mps._data[ind]
+            return self._tts._data[ind]
 
     def __len__(self):
         return len(self._data)
     def __setitem__(self,ind,it):
-        it=_flatten_mps(it)
+        it=_flatten_ttslice(it)
         if isinstance(ind,slice):
             if ind.step==1:
-                self.mps._data[ind]=it
+                self._tts._data[ind]=it
             else:
                 raise IndexError("Slicing with a step not equal to one not supported for TensorTrains")
         else: #single index i guess
-            self.mps._data[ind]=it
-        self._update_attributes()
+            self._tts._data[ind]=it
+        self._check_consistency()
     def __delitem__(self,ind):
-        del self.mps._data[ind]
-        self._update_attributes()
+        del self._tts._data[ind]
+        self._check_consistency()
     def __iter__(self):
-        return self.mps._data.__iter__()
+        return self._tts._data.__iter__()
 
 class TensorTrainSlice(TensorTrainBase,NDArrayOperatorsMixin):
     def __init__(self,matrices):
         self._data=matrices
-        self.M=_TensorTrainSliceData(self)
-        self._update_attributes()
-    def _update_attributes(self):
-        self.L=len(self._data)
-        if self.L<1:
-            raise ValueError("There must be at least one matrix in the TensorTrainSlice")
-        self.chi=tuple(m.shape[0] for m in self._data[1:])
-        self.cluster=tuple(m.shape[1:-1] for m in self._data)
+        self._check_consistency()
+    @property
+    def M(self):
+        return _TensorTrainSliceData(self)
+    @property
+    def shape(self):
         rank=len(self._data[0].shape)
-        for m in self._data:
-            if len(m.shape)!=rank:
-                raise ValueError("All matrices in a TensorTrainSlice need to have the same rank")
         shape=[self._data[0].shape[0]]
         shape.extend([_product(c[d] for c in self.cluster) for d in range(rank-2)])
         shape.append(self._data[-1].shape[-1])
-        self.shape=tuple(shape)
-        self.dtype=np.result_type(*self._data)
+        return tuple(shape)
+    @property
+    def cluster(self):
+        return tuple(m.shape[1:-1] for m in self._data)
+    @property
+    def chi(self):
+        return tuple(m.shape[0] for m in self._data[1:])
+    @property
+    def L(self):
+        return len(self._data)
+    @property
+    def dtype(self):
+        return np.result_type(*self._data) #maybe enforce all matrices to the same dtype eventually
+
+    def _check_consistency(self):
+        if self.L<1:
+            raise ValueError("There must be at least one matrix in the TensorTrainSlice")
+        rank=len(self._data[0].shape)
+        nd=self._data[0].shape[0]
+        for m in self._data:
+            if len(m.shape)!=rank:
+                raise ValueError("All matrices in a TensorTrainSlice need to have the same rank")
+            if m.shape[0]!=nd:
+                raise ValueError("The virtual bonds need to be consistent")
+            nd=m.shape[-1]
     @classmethod
     def frommatrices(cls,matrices):
         return cls(matrices)
@@ -114,35 +138,42 @@ class TensorTrainSlice(TensorTrainBase,NDArrayOperatorsMixin):
         ret=HANDLER_UFUNC_SLICE[(ufunc,method)](*args,**kwargs)
         return ret
 
-class _TensorTrainArrayData:
-    def __init__(self,tta):
-        self._tta=tta
-        self._M=tta._tts.M
-    def __getitem__(self,ind):
-        return self._M.__getitem__(self,ind)
-    def __len__(self):
-        return self._M.__len__(self)
+class _TensorTrainArrayData(_TensorTrainSliceData):
     def __setitem__(self,ind,it):
-        self._M.__setitem__(ind,it)
-        self._tta._update_attributes()
+        super().__setitem__(ind,it)
+        if self._tts._data[0].shape[0]!=1 or self._tts._data[-1].shape[-1]!=1:
+            raise ValueError("TensorTrainArrays cannot have a non-contracted boundary")
+
     def __delitem__(self,ind):
-        self._M.__delitem__(ind)
-        self._tta._update_attributes()
-    def __iter__(self):
-        return self._M.__iter__()
+        super().__delitem__(ind)
+        if self._tts._data[0].shape[0]!=1 or self._tts._data[-1].shape[-1]!=1:
+            raise ValueError("TensorTrainArrays cannot have a non-contracted boundary")
+
 class TensorTrainArray(TensorTrainBase,NDArrayOperatorsMixin):
     def __init__(self,tts):
         self._tts=tts
-        self.M=_TensorTrainArrayData(self)
-        self._update_attributes()
-    def _update_attributes(self):
-        if self._tts.shape[0]!=1 or self._tts.shape[-1]!=1:
+        if tts._data[0].shape[0]!=1 or tts._data[-1].shape[-1]!=1:
             raise ValueError("TensorTrainArrays cannot have a non-contracted boundary")
-        self.shape=self._tts.shape[1:-1]
-        self.cluster=self._tts.cluster
-        self.dtype=self._tts.dtype
-        self.L=self._tts.L
-        self.chi=self._tts.chi
+    @property
+    def M(self):
+        return _TensorTrainArrayData(self._tts)
+    @property
+    def shape(self):
+        return self._tts.shape[1:-1]
+    @property
+    def cluster(self):
+        return self._tts.cluster
+
+    @property
+    def chi(self):
+        return self._tts.chi
+    @property
+    def dtype(self):
+        return self._tts.dtype
+    @property
+    def L(self):
+        return self._tts.L
+
     def __array__(self,dtype=None):
         return np.asarray(self.todense(),dtype)
     # @classmethod
