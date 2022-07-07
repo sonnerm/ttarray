8bbd3ec489ec5e58ea1fd14d8f44a0a3e62dfb25
Michael Sonner <sonnerm@gmail.com>
2022-07-07 13:15:30 +0200

    modernized contraction

--- ttarray/core/contraction.py
+++ ttarray/core/contraction.py
@@ -1,62 +1,62 @@
 import numpy as np
+from .creation import asarray
 from .dispatch import implement_function,implement_ufunc
 from .base import TensorTrainBase
 from .slice import TensorTrainSlice
 from .array import TensorTrainArray
 from .reshape import recluster
 import ttarray.raw as raw
-@implement_function()
-def tensordot(x,y,axes=2,out=None):
+def tensordot_slice(x,y,axes=2,out=None):
+    '''
+        tensordot, but with slice-aware ordering of uncontracted indices
+    '''
+    pass
+def _format_axes(axes,xr,yr):
     if isinstance(axes,(int,np.integer)):
         axes=(tuple(range(-axes,0)),tuple(range(0,axes)))
     elif isinstance(axes[0],(int,np.integer)):
         axes=((axes[0],),(axes[1],))
-    axes=(tuple(c if c>=0 else c+len(x.shape) for c in axes[0]),tuple(c if c>=0 else c+len(y.shape) for c in axes[1]))
-    if not isinstance(x,TensorTrainBase) and not isinstance(y,TensorTrainBase):
-        return NotImplemented #not my job then
-    if not isinstance(x,TensorTrainBase):
-        return np.tensordot(np.array(x,like=y),y,axes)
-    if not isinstance(y,TensorTrainBase):
-        return np.tensordot(x,np.array(y,like=x),axes)
-
-    if isinstance(x,TensorTrainSlice) and isinstance(y,TensorTrainSlice):
-        raise NotImplementedException("tensordot operations between two TensorTrainSlice are not supported yet.")
-    elif isinstance(x,TensorTrainSlice):
-        # promote to array if at least one is an array
-        x=array(x)
-    if isinstance(y,TensorTrainSlice):
-        y=array(y)
+    axes=(tuple(c if c>=0 else c+xr for c in axes[0]),tuple(c if c>=0 else c+yr for c in axes[1]))
+    return axes
+@implement_function()
+def tensordot(x,y,axes=2,out=None):
+    if out is not None:
+        raise NotImplementedError("out is not yet supported")
+    axes=_format_axes(axes,len(x.shape),len(y.shape))
+    y=asarray(y)
+    x=asarray(x)
     yc=tuple(tuple(ycc[i] for i in axes[1]) for ycc in y.cluster)
     x=recluster(x,yc,axes=axes[0],copy=True)
     ntt=raw.tensordot(x.tomatrices(),y.tomatrices(),axes)
     return x.__class__.frommatrices(ntt)
 @implement_function()
 def dot(x,y,out=None):
-    if not isinstance(x,TensorTrainBase) and isinstance(y,TensorTrainBase):
-        return NotImplemented
     if len(x.shape)==1 and len(y.shape)==1:
         return tensordot(x,y,axes=((0,),(0,)),out=out)
     else:
-        raise ValueError("shapes are not consistent")
+        return tensordot(x,y,axes=((len(x.shape)-1,),(len(y.shape)-2,)))
 @implement_function()
 def vdot(x,y,out=None):
-    if not isinstance(x,TensorTrainBase) and isinstance(y,TensorTrainBase):
-        return NotImplemented
     if len(x.shape)==1 and len(y.shape)==1:
         return tensordot(x.conj(),y,axes=((0,),(0,)),out=out)
     else:
-        raise ValueError("shapes are not consistent")
-
+        return tensordot(x.conj(),y,axes=((len(x.shape)-1,),(len(y.shape)-2,)),out=out)
 
 @implement_ufunc("matmul","__call__")
 def matmul(x,y):
-    if not isinstance(x,TensorTrainBase) and isinstance(y,TensorTrainBase):
-        return NotImplemented
-    if len(x.shape)==1 and len(y.shape)==1:
+    if len(y.shape)==0 or len(x.shape)==0:
+        raise ValueError("Scalar multiplication with matmul is not allowed!")
+    if len(y.shape)!=1 and len(y.shape)!=2:
+        raise NotImplementedError("shapes other then 1-D and 2-D are not yet supported")
+    if len(x.shape)==1:
         return tensordot(x,y,axes=((0,),(0,)))
+    elif len(x.shape)==2:
+        return tensordot(x,y,axes=((1,),(0,)))
     else:
-        return tensordot(x,y,axes=((len(x.shape)-1,),(len(y.shape)-2,)))
-
+        raise NotImplementedError("shapes other then 1-D and 2-D are not yet supported")
+# @implement_function
+# def multi_dot():
+#     pass
 # @implement_function
 # def einsum(a,out=None):
 #     if not isinstance(x,TensorTrainBase) and isinstance(y,TensorTrainBase):
@@ -65,6 +65,7 @@ def matmul(x,y):
 #         return tensordot(x.conj(),y,axes=((0,),(0,)),out=out)
 #     else:
 #         raise ValueError("shapes are not consistent")
+#
 @implement_function()
 def trace(a,offset=0, axis1=0,axis2=1,dtype=None,out=None):
     if offset!=0:
@@ -81,9 +82,11 @@ def trace(a,offset=0, axis1=0,axis2=1,dtype=None,out=None):
         raise ValueError("axis2 out of bounds: %i (rank: %i)"%(axis2,len(a.shape)))
     if isinstance(a,TensorTrainSlice):
         if axis1==0 or axis1==len(a.shape)-1 or axis2==0 or axis2==len(a.shape)-1:
-            raise NotImplementedError("Tracing the boundary is not yet supported for TensorTrainSlices")
+            return trace(array(a),offset,axis1,axis2,dtype,out)
+        a=a.recluster([(x[axis2],x[axis2]) for x in a.cluster],[axis1,axis2])
         return TensorTrainSlice.frommatrices_unchecked([np.trace(x,0,axis1+1,axis2+1,dtype) for x in a.tomatrices_unchecked()])
     elif isinstance(a,TensorTrainArray):
+        a=a.recluster([(x[axis2],x[axis2]) for x in a.cluster],[axis1,axis2])
         return TensorTrainArray.frommatrices_unchecked([np.trace(x,0,axis1+1,axis2+1,dtype) for x in a.tomatrices_unchecked()])
     else:
-        return NotImplemented
+        return np.trace(a,offset,axis1,axis2,dtype,out)
